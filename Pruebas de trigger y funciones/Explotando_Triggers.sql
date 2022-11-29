522e345362ab30025d6c23812edcb17e9375f849
AlexRodriguezZA <alekeyn1999@gmail.com>
2022-11-29 12:26:11 -0300

    Cambiosss

--- Pruebas de trigger y funciones/Explotando_Triggers.sql
+++ Pruebas de trigger y funciones/Explotando_Triggers.sql
@@ -27,3 +27,48 @@ INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod)
 			VALUES (6000,200,1,13);
 
 select * from producto where idprod = 13;
+
+
+-------------------------------------------------------------------------------------------
+
+--Aumento del stock, cuando agregamos mas productos a travez de la tabla de compraprod
+select nombre,stock from producto where idprod = 1;
+
+INSERT INTO compraprod (cantidad,fecha,idprod)
+VALUES (34,current_date,1);
+
+select nombre,stock from producto where idprod = 1;
+-------------------------------------------------------------------------------------------
+--Descuento del stock, cuando finalizamos la compra lo que debemos hacer es descontar el stock 
+--del producto
+
+select nombre,stock from producto where idprod = 1;
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6000,20,1,1);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(1));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(1));
+
+select nombre,stock from producto where idprod = 1;
+
+-------------------------------------------------------------------------------------------
+--Con el trigger de actulizar automaticamente el stock podemos aumentar el promedio del 
+--stock automaticamente en la tabla del producto
+
+select nombre,stock,promediocalificacion,idprod from producto where idprod = 6
+
+---Primero tenemos que compralo antes de calificarlo
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6000,1,1,6);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(1));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(1));
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (3,funcion_encontrar_dni_del_idcarrito(1),6);
+
+select nombre,stock,promediocalificacion,idprod from producto where idprod = 6
+
