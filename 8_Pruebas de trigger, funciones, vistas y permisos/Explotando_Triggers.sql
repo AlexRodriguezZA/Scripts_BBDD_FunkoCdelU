eb6aeea943726610a81ff2fdc844370be26d5d2d
AlexRodriguezZA <alekeyn1999@gmail.com>
2022-11-29 17:21:18 -0300

    Cambios

--- 8_Pruebas de trigger, funciones, vistas y permisos/Explotando_Triggers.sql
+++ 8_Pruebas de trigger, funciones, vistas y permisos/Explotando_Triggers.sql
@@ -1,5 +1,7 @@
 --EXPLOTANDO TRIGGERS
---Trigger de calificar funko, este mismo se va a dispara si el usuario no compro el funko
+
+
+--Trigger de calificar funko, este mismo se va a dispara si el usuario no compro el funko y quiere calificar un producto
 
 INSERT INTO calificacion (calificacion,dni,idprod) 
 VALUES (5,funcion_encontrar_dni_del_idcarrito(9),10);
@@ -42,7 +44,9 @@ select nombre,stock from producto where idprod = 1;
 --Descuento del stock, cuando finalizamos la compra lo que debemos hacer es descontar el stock 
 --del producto
 
+
 select nombre,stock from producto where idprod = 1;
+
 
 INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
 			VALUES (6000,20,1,1);
@@ -57,7 +61,7 @@ select nombre,stock from producto where idprod = 1;
 --Con el trigger de actulizar automaticamente el stock podemos aumentar el promedio del 
 --stock automaticamente en la tabla del producto
 
-select nombre,stock,promediocalificacion,idprod from producto where idprod = 6
+select nombre,stock,promediocalificacion,idprod from producto where idprod = 6;
 
 ---Primero tenemos que compralo antes de calificarlo
 INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
@@ -70,5 +74,5 @@ SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(1));
 INSERT INTO calificacion (calificacion,dni,idprod) 
 VALUES (3,funcion_encontrar_dni_del_idcarrito(1),6);
 
-select nombre,stock,promediocalificacion,idprod from producto where idprod = 6
+select nombre,stock,promediocalificacion,idprod from producto where idprod = 6;
 
