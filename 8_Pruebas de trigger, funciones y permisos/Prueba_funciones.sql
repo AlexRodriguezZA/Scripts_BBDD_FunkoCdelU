e662d48fc33c6e4b08691033dca477397a8d86a0
AlexRodriguezZA <alekeyn1999@gmail.com>
2022-11-29 12:52:34 -0300

    Cambios

--- 8_Pruebas de trigger, funciones y permisos/Prueba_funciones.sql
+++ 8_Pruebas de trigger, funciones y permisos/Prueba_funciones.sql
@@ -1,5 +1,5 @@
 select Get_compras_del_dia(CURRENT_DATE);
 select Get_cantidad_productos_de_cada_categoria(2);
 select cantidadDeProddeCadaCat(1);
-select Get_compras_fecha_dada('2022-11-28');
+select Get_compras_fecha_dada(CURRENT_DATE);
 
