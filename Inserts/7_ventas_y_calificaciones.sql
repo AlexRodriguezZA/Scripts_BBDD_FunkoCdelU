91d4d481f7efdc532dc3179a7d912f7337f793e7
AlexRodriguezZA <alekeyn1999@gmail.com>
2022-11-29 10:56:48 -0300

    cambios wey

---
+++ Inserts/7_ventas_y_calificaciones.sql
@@ -0,0 +1,296 @@
+--------------------------VENTAS REALIZADAS---------------------------------------------
+
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6000,2,1,1);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6100,2,1,2);
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (4000,2,1,4);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6000,2,1,13);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(1));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(1));
+
+
+----------------------------------------------------------------------------------------
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6400,2,2,6);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6400,2,2,3);
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5500,2,2,5);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5500,2,2,12);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(2));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(2));
+
+----------------------------------------------------------------
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6400,2,3,1);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6400,2,3,2);
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5500,2,3,7);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5500,2,3,4);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(3));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(3));
+
+
+--------------------------------------------------------------------------------------------------
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6000,2,4,1);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6100,2,4,2);
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (4000,2,4,4);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6000,2,4,13);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(4));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(4));
+
+
+----------------------------------------------------------------------------------------
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6400,2,5,6);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6400,2,5,3);
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5500,2,5,5);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5500,2,5,12);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(5));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(5));
+
+----------------------------------------------------------------
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6400,2,6,10);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6400,2,6,1);
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5500,2,6,6);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5500,2,6,5);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(6));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(6));
+
+-------------------------------------------------------------------------------------
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6000,2,7,11);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6100,2,7,12);
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (4000,2,7,3);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6000,2,7,13);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(7));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(7));
+
+
+----------------------------------------------------------------------------------------
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6400,2,8,14);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6400,2,8,10);
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5500,2,8,11);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5500,2,8,12);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(8));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(8));
+
+----------------------------------------------------------------
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6400,2,9,1);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6400,2,9,2);
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5500,2,9,7);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5500,2,9,4);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(9));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(9));
+
+-----------------------------------------------------------------------------------
+
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6000,2,10,1);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6100,2,10,2);
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (4000,2,10,14);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6000,2,10,3);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(10));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(10));
+
+
+----------------------------------------------------------------------------------------
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6400,2,11,6);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6400,2,11,3);
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5500,2,11,5);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5500,2,11,12);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(11));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(11));
+
+----------------------------------------------------------------
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6400,2,12,1);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (6400,2,12,2);
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5500,2,12,7);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5500,2,12,4);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(12));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(12));
+
+--------------------------------------------------------------------------------
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (10000,2,1,7);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5000,2,1,8);
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (4000,2,1,12);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (10000,2,1,9);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(1));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(1));
+--------------------------------------------------------------------------------
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (10000,2,8,7);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5000,2,8,8);
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (4000,2,8,12);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (10000,2,8,9);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(8));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(8));
+
+----------------------------------------------------------------------------
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (1000,2,1,11);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (5600,2,1,9);
+
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (1000,2,1,2);
+INSERT INTO lineacarrito (precio,cantidaddecadaprod,idcarrito,idprod) 
+			VALUES (1000,2,1,10);
+
+
+SELECT confirmar_comprar_del_carrito(funcion_encontrar_dni_del_idcarrito(1));
+SELECT confirmar_estado_de_venta(funcion_encontrar_dni_del_idcarrito(1));
+
+
+-----------------------------------------------------------------------------
+
+--------------------------CALIFICACIONES---------------------------------------------
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (4,funcion_encontrar_dni_del_idcarrito(1),9);
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (2,funcion_encontrar_dni_del_idcarrito(1),1);
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (4,funcion_encontrar_dni_del_idcarrito(1),10);
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (5,funcion_encontrar_dni_del_idcarrito(1),11);
+
+---------------------------------------------------------------------
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (3,funcion_encontrar_dni_del_idcarrito(8),12);
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (2,funcion_encontrar_dni_del_idcarrito(8),11);
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (4,funcion_encontrar_dni_del_idcarrito(8),10);
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (5,funcion_encontrar_dni_del_idcarrito(8),14);
+
+---------------------------------------------------------------------
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (3,funcion_encontrar_dni_del_idcarrito(12),1);
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (2,funcion_encontrar_dni_del_idcarrito(12),2);
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (4,funcion_encontrar_dni_del_idcarrito(12),7);
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (5,funcion_encontrar_dni_del_idcarrito(12),4);
+---------------------------------------------------------------------
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (5,funcion_encontrar_dni_del_idcarrito(9),1);
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (5,funcion_encontrar_dni_del_idcarrito(9),2);
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (5,funcion_encontrar_dni_del_idcarrito(9),7);
+
+INSERT INTO calificacion (calificacion,dni,idprod) 
+VALUES (5,funcion_encontrar_dni_del_idcarrito(9),4);
+
+
