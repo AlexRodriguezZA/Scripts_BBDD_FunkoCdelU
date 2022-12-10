1f0a4e205e7818ab3c0afe5ccb609fc8449dd23c
AlexRodriguezZA <alekeyn1999@gmail.com>
2022-12-10 10:41:22 -0300

    Explotando dominios y restricciones

--- 8_Pruebas de trigger, funciones, vistas y permisos/Prueba_funciones.sql
+++ 8_Pruebas de trigger, funciones, vistas y permisos/Prueba_funciones.sql
@@ -1,5 +1,4 @@
 select Get_compras_del_dia(CURRENT_DATE);
 select Get_cantidad_productos_de_cada_categoria(2);
-select cantidadDeProddeCadaCat(1);
 select Get_compras_fecha_dada(CURRENT_DATE);
 select Get_favoritos_usuario(42070594);
