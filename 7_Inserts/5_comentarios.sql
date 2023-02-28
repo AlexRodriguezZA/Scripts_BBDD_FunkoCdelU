adaafbe89a41115b5f04bf33d7e86e32aa030c7d
AlexRodriguezZA <alekeyn1999@gmail.com>
2023-02-27 22:20:00 -0300

    Cambios en el insert

--- 7_Inserts/5_comentarios.sql
+++ 7_Inserts/5_comentarios.sql
@@ -1,57 +1,57 @@
-INSERT INTO comentarios (hora,contenido,fecha,dni,idprod) 
-VALUES (current_time(0),'MUY LINDO PRODUCTO CHIQUES',CURRENT_DATE,42070594,1);
+INSERT INTO comentarios (contenido,fecha,dni,idprod) 
+VALUES ('MUY LINDO PRODUCTO CHIQUES','12/01/2023',42070594,1);
 
-INSERT INTO comentarios (hora,contenido,fecha,dni,idprod) 
-VALUES (current_time(0),'Aprobado',CURRENT_DATE,42070594,2);
+INSERT INTO comentarios (contenido,fecha,dni,idprod) 
+VALUES ('Aprobado','12/01/2023',42070594,2);
 
-INSERT INTO comentarios (hora,contenido,fecha,dni,idprod) 
-VALUES (current_time(0),'Tienen por mayor??',CURRENT_DATE,42070594,3);
+INSERT INTO comentarios (contenido,fecha,dni,idprod) 
+VALUES ('Tienen por mayor??','12/01/2023',42070594,3);
 
-INSERT INTO comentarios (hora,contenido,fecha,dni,idprod) 
-VALUES (current_time(0),'Nasheee',CURRENT_DATE,42070594,4);
-
-------------------------------------------------------------
-
-INSERT INTO comentarios (hora,contenido,fecha,dni,idprod) 
-VALUES (current_time(0),'BUen producto xd',CURRENT_DATE,44234567,1);
-
-INSERT INTO comentarios (hora,contenido,fecha,dni,idprod) 
-VALUES (current_time(0),'Aprobado',CURRENT_DATE,44234567,2);
-
-INSERT INTO comentarios (hora,contenido,fecha,dni,idprod) 
-VALUES (current_time(0),'Me llego tarde ahre',CURRENT_DATE,44234567,3);
-
-INSERT INTO comentarios (hora,contenido,fecha,dni,idprod) 
-VALUES (current_time(0),'Ta zarpado',CURRENT_DATE,44234567,4);
+INSERT INTO comentarios (contenido,fecha,dni,idprod) 
+VALUES ('Nasheee','12/01/2023',42070594,4);
 
 ------------------------------------------------------------
 
-INSERT INTO comentarios (hora,contenido,fecha,dni,idprod) 
-VALUES (current_time(0),'BUena atencion gracias',CURRENT_DATE,45450594,1);
+INSERT INTO comentarios (contenido,fecha,dni,idprod) 
+VALUES ('BUen producto xd','12/01/2023',44234567,1);
 
-INSERT INTO comentarios (hora,contenido,fecha,dni,idprod) 
-VALUES (current_time(0),'MUy lindo eh',CURRENT_DATE,45450594,2);
+INSERT INTO comentarios (contenido,fecha,dni,idprod) 
+VALUES ('Aprobado','12/01/2023',44234567,2);
 
-INSERT INTO comentarios (hora,contenido,fecha,dni,idprod) 
-VALUES (current_time(0),'Descuento man???',CURRENT_DATE,45450594,3);
+INSERT INTO comentarios (contenido,fecha,dni,idprod) 
+VALUES ('Me llego tarde ahre','12/01/2023',44234567,3);
 
-INSERT INTO comentarios (hora,contenido,fecha,dni,idprod) 
-VALUES (current_time(0),'No me gusto',CURRENT_DATE,45450594,4);
+INSERT INTO comentarios (contenido,fecha,dni,idprod) 
+VALUES ('Ta zarpado','12/01/2023',44234567,4);
+
+------------------------------------------------------------
+
+INSERT INTO comentarios (contenido,fecha,dni,idprod) 
+VALUES (current_time(0),'BUena atencion gracias','12/01/2023',45450594,1);
+
+INSERT INTO comentarios (contenido,fecha,dni,idprod) 
+VALUES ('MUy lindo eh','12/01/2023',45450594,2);
+
+INSERT INTO comentarios (contenido,fecha,dni,idprod) 
+VALUES ('Descuento man???','12/01/2023',45450594,3);
+
+INSERT INTO comentarios (contenido,fecha,dni,idprod) 
+VALUES ('No me gusto','12/01/2023',45450594,4);
 
 
 
 ------------------------------------------------------------
 
-INSERT INTO comentarios (hora,contenido,fecha,dni,idprod) 
-VALUES (current_time(0),'BUena atencion gracias xddd',CURRENT_DATE,31450594,1);
+INSERT INTO comentarios (contenido,fecha,dni,idprod) 
+VALUES ('BUena atencion gracias xddd','12/01/2023',31450594,1);
 
-INSERT INTO comentarios (hora,contenido,fecha,dni,idprod) 
-VALUES (current_time(0),'Excelente calidad',CURRENT_DATE,31450594,2);
+INSERT INTO comentarios (contenido,fecha,dni,idprod) 
+VALUES ('Excelente calidad','12/01/2023',31450594,2);
 
-INSERT INTO comentarios (hora,contenido,fecha,dni,idprod) 
-VALUES (current_time(0),'MUy recomendable',CURRENT_DATE,31450594,3);
+INSERT INTO comentarios (contenido,fecha,dni,idprod) 
+VALUES ('MUy recomendable','12/01/2023',31450594,3);
 
-INSERT INTO comentarios (hora,contenido,fecha,dni,idprod) 
-VALUES (current_time(0),'No me gusto xddd',CURRENT_DATE,31450594,4);
+INSERT INTO comentarios (contenido,fecha,dni,idprod) 
+VALUES ('No me gusto xddd','12/01/2023',31450594,4);
 
 
